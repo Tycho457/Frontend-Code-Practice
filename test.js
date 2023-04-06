129306cb4df515abb6b33393d71ae0ea55d8f9e8
Tycho <1756518455@qq.com>
2023-04-06 20:15:59 +0800

    更新test.js

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-const obj = {
-    name: "小明",
-    age: 19
-}
-instanceof obj
-console.log(1)
+const math = require('mathjs');
+const a = math.bignumber('0.1');
+const b = math.bignumber('0.2');
+const result = math.add(a, b);
+console.log(result.toString()); // '0.3'
