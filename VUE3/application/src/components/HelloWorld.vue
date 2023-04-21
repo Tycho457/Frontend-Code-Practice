b001821685aa44abd0a92e7a45393282c8850344
Tycho <1756518455@qq.com>
2023-04-21 11:47:05 +0800

    更新 自定义组件移动案例

--- VUE3/application/src/components/HelloWorld.vue
+++ VUE3/application/src/components/HelloWorld.vue
@@ -1,62 +1,52 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div v-move class="box">
+    <div class="header"></div>
+    <div>内容</div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
+<script lang="ts" setup>
+import { Directive } from "vue";
+const vMove: Directive = {
+  // mounted 钩子函数会在元素挂载到 DOM 后触发
+  mounted(el: HTMLElement) {
+    let moveEl = el.firstElementChild as HTMLElement;
+    // 鼠标按下事件
+    const mouseDown = (e: MouseEvent) => {
+      console.log(e.clientX, e.clientY, "----起始", el.offsetLeft);
+      let X = e.clientX - el.offsetLeft;
+      let Y = e.clientY - el.offsetTop;
+      // 处理鼠标移动事件
+      const move = (e: MouseEvent) => {
+        el.style.left = e.clientX - X + "px";
+        el.style.top = e.clientY - Y + "px";
+        console.log(e.clientX, e.clientY, "----改变");
+      };
+      // 监听鼠标移动和松开
+      document.addEventListener("mousemove", move);
+      document.addEventListener("mouseup", () => {
+        document.removeEventListener("mousemove", move);
+      });
+    };
+    // 监听目标鼠标按下事件
+    moveEl.addEventListener("mousedown", mouseDown);
   },
-});
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.box {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 200px;
+  height: 200px;
+  border: 1px solid #ccc;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.header {
+  height: 20px;
+  background: black;
+  cursor: move;
 }
 </style>
